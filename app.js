c844d4f517df356078cf15cd1361e28cb22f5c5d
Nyapal Gatkuoth <nyapalgatkuoth@nyapals-mbp.attlocal.net>
2019-01-10 15:43:34 -0800

    added donors & families

--- app.js
+++ app.js
@@ -6,7 +6,13 @@ app.engine('hbs', exphbs({defaultLayout: 'main', extname: '.hbs'}));
 app.set('view engine', 'hbs');
 
 app.get('/', (req, res) => {
-    res.render('home', {msg: 'hbs'})
+    res.render('home')
+})
+app.get('/donors', (req, res) => {
+    res.render('donors')
+})
+app.get('/families', (req, res) => {
+    res.render('families')
 })
 
 app.listen(3000, () => {
